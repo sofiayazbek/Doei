944f6313675aa048c2268d4a9a2d0f76411e8686
SOFIA AINHOA <88836758+sofiayazbek@users.noreply.github.com>
2021-11-18 13:56:12 +0000

    .

--- src/views/pagamento.vue
+++ src/views/pagamento.vue
@@ -4,12 +4,15 @@
       <h1 class="title">Finalizar pagamento</h1>
       <hr style="width:70px; background-color:#ff0000">
       
-      <p class= "is-size-6">Nosso modelo de pagamento por enquanto é o pix, estamos em fase de desenvolvimento e logo teremos outras formas para melhor atende-los. <br></p>
+      <p class= "is-size-6">Nosso modelo de pagamento por enquanto é o pix, estamos em fase de desenvolvimento e logo teremos outras formas para melhor atendê-los. <br></p>
       <br>
       <br>
     
       <h1 class="title has-text-centered"> PIX: 106.576.769-26 </h1>
-
+      <br>
+      <br>
+      <br>
+      <p class= "is-size-6">Após o pagamento, envie seu comprovante para o nosso WhatsApp: <br></p>
       <br>
       <a>
         <button class="button is-danger">Enviar comprovante</button>

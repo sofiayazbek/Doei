6b575ba40f2d456bd6dd8d7b1d23a7c0a0c74eb2
BRUNA SCHWARTZ <88836751+brunaschwartz@users.noreply.github.com>
2021-11-18 13:26:25 +0000

    pagamento

--- src/views/pagamento.vue
+++ src/views/pagamento.vue
@@ -3,10 +3,13 @@
 
       <h1 class="title">Finalizar pagamento</h1>
       <hr style="width:70px; background-color:#ff0000">
+      
+      <p class= "is-size-6">Nosso modelo de pagamento por enquanto é o pix, estamos em fase de desenvolvimento e logo teremos outras formas para melhor atende-los. <br></p>
       <br>
-
+      <br>
     
-        
+      <h1 class="title has-text-centered"> PIX: 106.576.769-26 </h1>
+
       <br>
       <a>
         <button class="button is-danger">Continuar</button>

32f6dd40ea5869418f16d217a702b70f3eaa0507
SOFIA AINHOA <88836758+sofiayazbek@users.noreply.github.com>
2021-12-02 11:50:28 +0000

    link whats bru

--- src/components/vfooterbar.vue
+++ src/components/vfooterbar.vue
@@ -21,7 +21,7 @@
               <i class="fab fa-instagram"></i>
           </a> 
           &nbsp;
-          <a href="https://wa.me/message/4JFYOXXBORT6P1" target="_blank" class="is-size-2">
+          <a href="https://wa.me/qr/UVG2BWK2NWHSF1" target="_blank" class="is-size-2">
               <i class="fab fa-whatsapp"></i>
           </a>  
         </div>          
